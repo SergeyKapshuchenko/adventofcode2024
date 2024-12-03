0e9c34b6c8722dcfed68dde1a25592836e697caf
s.kapshuchenko <s.kapshuchenko@smartweb.com.ua>
2024-12-03 14:56:15 +0200

    day1 second part

--- src/day1/solution.py
+++ src/day1/solution.py
@@ -1,7 +1,17 @@
+from collections import Counter
+
+
 def find_distance(first: list[int], second: list[int]) -> int:
+    """part 1"""
     first = sorted(first)
     second = sorted(second)
     return sum(abs(first[i] - second[i]) for i in range(len(first)))
+
+
+def get_similarity_score(first: list[int], second: list[int]) -> int:
+    """part 2"""
+    counter = Counter(second)
+    return sum(i * counter[i] for i in first)
 
 
 def read_input(file_path: str) -> tuple[list[int], list[int]]:
@@ -14,3 +24,6 @@ if __name__ == "__main__":
     first_list, second_list = read_input("input.txt")
     distance = find_distance(first_list, second_list)
     print(distance)
+
+    similarity_score = get_similarity_score(first_list, second_list)
+    print(similarity_score)
