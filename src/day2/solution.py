86cb71ad42043166ac0e330f8c3fedf3fbf667cc
s.kapshuchenko <s.kapshuchenko@smartweb.com.ua>
2024-12-03 19:00:21 +0200

    day2 part 2

--- src/day2/solution.py
+++ src/day2/solution.py
@@ -8,8 +8,18 @@ def is_safe(report: list[int]) -> bool:
     return True
 
 
+def is_safe_with_allowed_bad_level(report: list[int]) -> bool:
+    combinations = [report[:i] + report[i + 1 :] for i in range(len(report))]
+    combinations.append(report)
+    return any(is_safe(comb) for comb in combinations)
+
+
 def count_safe_reports(reports: list[list[int]]) -> int:
     return sum(is_safe(report) for report in reports)
+
+
+def count_safe_reports_with_allowed_bad_level(reports: list[list[int]]) -> int:
+    return sum(is_safe_with_allowed_bad_level(report) for report in reports)
 
 
 def read_input(file_path: str) -> list[list[int]]:
@@ -19,5 +29,9 @@ def read_input(file_path: str) -> list[list[int]]:
 
 if __name__ == "__main__":
     reports = read_input("input.txt")
-    safe_reports_count = count_safe_reports(reports)
-    print(safe_reports_count)
+
+    # part 1
+    print(count_safe_reports(reports))
+
+    # part 2
+    print(count_safe_reports_with_allowed_bad_level(reports))
